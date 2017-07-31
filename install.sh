d41cc4c26e542a84f0751c4c4acc2ac501ce733d
Kitamu <ymyk6602@gmail.com>
2017-08-01 00:06:01 +0900

    add auto-complete settings

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# This file is for installing dotfiles in local machine.
+# For installation, run below command;
+# >> source install.sh
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     ln -snfv ~/dotfiles/"$f" ~/
 done
+
+
+## emacs settings ##
+# create backup directory
+if [ ! -d ~/backups ]; then
+    mkdir ~/backups
+fi
+
+    
