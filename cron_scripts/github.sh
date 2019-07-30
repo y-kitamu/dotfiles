013e8ab48ab81b0f3d3edf58f8c3f980024133bb
Kitamura <yusuke.sizebook.kitamura@gmail.com>
2019-07-30 22:25:10 +0900

    cron commit 20190730

--- cron_scripts/github.sh
+++ cron_scripts/github.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+set USERNAME "y-kitamu"
+set PASSWORD "ymyk6422"
+
 # git hub から fetch, push
 
 cd /home/kitamura/dotfiles/
@@ -10,4 +13,21 @@ git fetch
 git merge origin/master
 
 git add .
-git commit -m "cron commit ${date '+%Y%m%d'}"
+git commit -m "cron commit `date '+%Y%m%d'`"
+
+set timeout 5
+spawn git push -u origin master
+expect {
+    "Username for 'https://github.com':" {
+        send "${USERNAME}\n"
+        exp_continue
+    }
+    "Password for 'https://${}@github.com':" {
+        send "${PASSWORD}\n"
+    }
+}
+expect {
+    "\\\$" {
+        exit 0
+    }
+}
