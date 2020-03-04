3fede9d8fea6c420955c17d2e88ac1152102a96a
Kitamura <ymyk6602@gmail.com>
2020-03-04 23:36:26 +0900

    refactor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -507,7 +507,7 @@
 (use-package yatex                                                                                   
   :mode ("\\.tex\\'" . yatex-mode)
   :config
-  (setq YaTeX-electric-indent-mode t)
+  ;; (setq YaTeX-electric-indent-mode t) ; 改行時の自動インデントの設定
   (let (
         (prefix "docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja ")
         (cmds '(
@@ -518,29 +518,3 @@
                 YaTeX-dvipdf-command
                 )))
     (cl-loop for cmd in cmds collect (set cmd (concat prefix (eval cmd))))))
-;; (use-package yatex
-;;   :ensure t
-;;   :init
-;;   (setq YaTeX-inhibit-prefix-letter t)    ;; yatex.elを読み込む前に設定が必要らしい
-;;   :mode
-;;   (("\\.tex\\'" . yatex-mode)
-;;    ("\\.ltx\\'" . yatex-mode)
-;;    ("\\.cls\\'" . yatex-mode)
-;;    ("\\.sty\\'" . yatex-mode)
-;;    ("\\.clo\\'" . yatex-mode)
-;;    ("\\.bbl\\'" . yatex-mode))
-;;   :config
-;;   (setq YaTeX-kanji-code nil)
-;;   (setq YaTeX-latex-message-code 'utf-8)
-;;   (setq YaTeX-use-AMS-LaTeX t)
-;;   (setq YaTeX-dvi2-command-ext-alist
-;;         '(("TeXworks\\|texworks\\|texstudio\\|mupdf\\|SumatraPDF\\|Preview\\|Skim\\|TeXShop\\|evince\\|okular\\|zathura\\|qpdfview\\|Firefox\\|firefox\\|chrome\\|chromium\\|Adobe\\|Acrobat\\|AcroRd32\\|acroread\\|pdfopen\\|xdg-open\\|open\\|start" . ".pdf")))
-;;   ;;(setq tex-command "platex -synctex=1")
-;;   (setq tex-command "pdflatex -synctex=1")
-;;   (setq dvi2-command "xdvi")
-;;   (when (equal system-type 'darwin)     ;; for Mac only
-;;     (setq dvi2-command "/usr/bin/open -a Skim")
-;;     (setq tex-pdfview-command "/usr/bin/open -a Skim"))
-;;   (setq bibtex-command "pbibtex")
-;;   (setq dviprint-command-format "dvipdfmx")
-;;   (add-hook 'yatex-mode-hook '(lambda () (auto-fill-mode -1)))) ;; 自動で改行しない
